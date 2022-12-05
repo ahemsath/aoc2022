925ab8f75bacd592cc795be7df319850afa96759
Alex Hemsath <alex.hemsath@gmail.com>
2022-12-04 17:55:19 -0800

    Day2, solution 2

--- Solvers/Day02.cs
+++ Solvers/Day02.cs
@@ -2,27 +2,57 @@
 
 namespace Solvers
 {
+    public enum Shape
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+
+    public enum Strategy
+    {
+        Lose,
+        Draw,
+        Win
+    }
+
     public class Day02 : ISolver
     {
         private string _inputFile;
 
-        private Dictionary<char, int> _shapeScores = new Dictionary<char, int>() { { 'X', 1 }, { 'Y', 2 }, { 'Z', 3 } };
+        private Dictionary<char, Shape> _opponentShapeMap = new Dictionary<char, Shape>() { { 'A', Shape.Rock }, { 'B', Shape.Paper }, { 'C', Shape.Scissors } };
+        private Dictionary<char, Shape> _myShapeMap = new Dictionary<char, Shape>() { { 'X', Shape.Rock }, { 'Y', Shape.Paper }, { 'Z', Shape.Scissors } };
+
+        private Dictionary<Shape, int> _shapeScores = new Dictionary<Shape, int>() { { Shape.Rock, 1 }, { Shape.Paper, 2 }, { Shape.Scissors, 3 } };
 
         // This data structure maps Rock-Paper-Scissors moves to scores
-        // The first char is the opponent's move.  A = Rock, B = Paper, C = Scissors
-        // The second char is our move.  X = Rock, Y = Paper, Z = Scissors
+        // First shape is opponent's, second is ours
         // 6 = win, 3 = draw, 0 = lose
-        private Dictionary<string, int> _moveScores = new Dictionary<string, int>()
+        private Dictionary<(Shape,Shape), int> _moveScores = new Dictionary<(Shape,Shape), int>()
         {
-            { "AX", 3 },
-            { "AY", 6 },
-            { "AZ", 0 },
-            { "BX", 0 },
-            { "BY", 3 },
-            { "BZ", 6 },
-            { "CX", 6 },
-            { "CY", 0 },
-            { "CZ", 3 }
+            { (Shape.Rock,Shape.Rock), 3 },
+            { (Shape.Rock,Shape.Paper), 6 },
+            { (Shape.Rock,Shape.Scissors), 0 },
+            { (Shape.Paper,Shape.Rock), 0 },
+            { (Shape.Paper,Shape.Paper), 3 },
+            { (Shape.Paper,Shape.Scissors), 6 },
+            { (Shape.Scissors,Shape.Rock), 6 },
+            { (Shape.Scissors,Shape.Paper), 0 },
+            { (Shape.Scissors,Shape.Scissors), 3 }
+        };
+
+        // This data structure encodes which shape I need to choose based on the opponent's shape and my strategy
+        private Dictionary<(Shape, Strategy), Shape> _stratMap = new Dictionary<(Shape, Strategy), Shape>()
+        {
+            { (Shape.Rock,Strategy.Lose), Shape.Scissors },
+            { (Shape.Rock,Strategy.Draw), Shape.Rock },
+            { (Shape.Rock,Strategy.Win), Shape.Paper },
+            { (Shape.Paper,Strategy.Lose), Shape.Rock },
+            { (Shape.Paper,Strategy.Draw), Shape.Paper },
+            { (Shape.Paper,Strategy.Win), Shape.Scissors },
+            { (Shape.Scissors,Strategy.Lose), Shape.Paper },
+            { (Shape.Scissors,Strategy.Draw), Shape.Scissors },
+            { (Shape.Scissors,Strategy.Win), Shape.Rock }
         };
 
         public Day02(string inputFile)
@@ -35,12 +65,12 @@ namespace Solvers
             int totalScore = 0;
             foreach (string line in File.ReadLines(_inputFile))
             {
-                char opponentPlay = line[0];
-                char myPlay = line[2];
+                Shape opponentPlay = _opponentShapeMap[line[0]];
+                Shape myPlay = _myShapeMap[line[2]];
                 int shapeScore = _shapeScores[myPlay];
-                var moveScore = _moveScores[$"{opponentPlay}{myPlay}"];
+                var moveScore = _moveScores[(opponentPlay,myPlay)];
                 int currentScore =  shapeScore + moveScore;
-                Console.WriteLine($"Opp play = {opponentPlay}, my play = {myPlay}, shape score = {shapeScore}, move score = {moveScore}");
+                //Console.WriteLine($"Opp play = {opponentPlay}, my play = {myPlay}, shape score = {shapeScore}, move score = {moveScore}");
                 totalScore += currentScore;
             }
             return totalScore;
@@ -48,7 +78,39 @@ namespace Solvers
 
         public int Answer2()
         {
-            return -1;
+            int totalScore = 0;
+            foreach (string line in File.ReadLines(_inputFile))
+            {
+                Shape opponentPlay = _opponentShapeMap[line[0]];
+                Shape myPlay;
+                Strategy myStrat;
+
+                // determine strategy
+                switch(line[2])
+                {
+                    case 'X':
+                        myStrat = Strategy.Lose;
+                        break;
+                    case 'Y':
+                        myStrat = Strategy.Draw;
+                        break;
+                    case 'Z':
+                        myStrat = Strategy.Win;
+                        break;
+                    default:
+                        myStrat = Strategy.Win;
+                        break;
+                }
+
+                myPlay = _stratMap[(opponentPlay,myStrat)];
+
+                int shapeScore = _shapeScores[myPlay];
+                var moveScore = _moveScores[(opponentPlay, myPlay)];
+                int currentScore = shapeScore + moveScore;
+                //Console.WriteLine($"Opp play = {opponentPlay}, my play = {myPlay}, shape score = {shapeScore}, move score = {moveScore}");
+                totalScore += currentScore;
+            }
+            return totalScore;
         }
     }
 }
