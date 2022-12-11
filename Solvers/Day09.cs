a0822654a65f02df6386862f7a8d8c3e7b2b33bd
Alex Hemsath <alex.hemsath@gmail.com>
2022-12-11 15:12:43 -0800

    Answer 2
    1. Refactor to have a rope of arbitratry length.
    2. Make sure we still get the correct answer for a rope of length 2 (answer 1)
    3. Get answer 2 (for a rope of length 10)
    4. Add debug input profile (launchSetting.json) to .gitignore

--- Solvers/Day09.cs
+++ Solvers/Day09.cs
@@ -15,58 +15,104 @@
 
         public string Answer1()
         {
-            HashSet<(int,int)> visited = new HashSet<(int,int)> ();
-
-            (int x,int y) head = (0,0);
-            (int x,int y) tail = (0,0);
-            visited.Add(tail);
-
-            foreach((char direction,int count) move in _moves)
-            {
-                Console.WriteLine($"Move head {move.direction} {move.count} times.");
-                for (int i = 0; i < move.count; i++)
-                {
-                    // update head pos
-                    head = MoveHead(head, move.direction);
-
-                    // update tail pos
-                    tail = FollowHead(head, tail);
-
-                    // store tail pos
-                    visited.Add(tail);
-                }
-            }
-            return visited.Count.ToString();
+            return GetTailVistedCount(2).ToString();
         }
 
         public string Answer2()
         {
-            return string.Empty;
+            return GetTailVistedCount(10).ToString();
         }
 
-        private (int, int) MoveHead((int x, int y) head, char direction)
+        private int GetTailVistedCount(int ropeLength)
         {
-            switch(direction)
+            var visited = new HashSet<(int, int)>();
+
+            var rope = new Rope(ropeLength);
+
+            visited.Add(rope.Tail);
+
+            foreach ((char direction, int count) move in _moves)
+            {
+                for (int i = 0; i < move.count; i++)
+                {
+                    // move rope
+                    rope.MoveHead(move.direction);
+
+                    // store tail pos
+                    visited.Add(rope.Tail);
+                }
+            }
+            return visited.Count;
+        }
+
+
+        private List<(char, int)> GetMoveList()
+        {
+            List<(char,int)> moves = new List<(char,int)>();
+            foreach (string line in File.ReadLines(_inputFile))
+            {
+                char direction = line[0];
+                int moveCount = int.Parse(line.Substring(2));
+                moves.Add((direction,moveCount));
+            }
+            return moves;
+        }
+    }
+
+    public class Rope
+    {
+        private List<(int x, int y)> _knots;
+        private int _length;
+
+        public (int x, int y) Head => _knots[0];
+        public (int x, int y) Tail => _knots[_length - 1];
+
+        public Rope(int length)
+        {
+            _length = length;
+            _knots = new List<(int x, int y)>();
+            for(int i = 0; i < _length; i++)
+            {
+                _knots.Add((0, 0)); // Initial position of each knot is 0,0
+            }
+        }
+
+        public void MoveHead(char direction)
+        {
+            // move head
+            _knots[0] = MoveKnot(Head,direction);
+
+            // update rest of knots
+            for (int i = 1; i < _length; i++)
+            {
+                _knots[i] = Follow(_knots[i - 1], _knots[i]);
+            }
+
+        }
+
+        private (int,int) MoveKnot((int x, int y) knot, char direction)
+        {
+            switch (direction)
             {
                 case 'U':
-                    return (head.x, head.y + 1);
+                    return (knot.x, knot.y + 1);
                 case 'D':
-                    return (head.x, head.y - 1);
+                    return (knot.x, knot.y - 1);
                 case 'L':
-                    return (head.x - 1, head.y);
+                    return (knot.x - 1, knot.y);
                 case 'R':
-                    return (head.x + 1, head.y);
+                    return (knot.x + 1, knot.y);
                 default:
                     throw new ArgumentException("Unknown direction " + direction);
             }
         }
 
-        private (int, int) FollowHead((int x, int y) head, (int x, int y) tail)
+        private (int, int) Follow((int x, int y) head, (int x, int y) tail)
         {
             int dx = head.x - tail.x;
             int dy = head.y - tail.y;
 
-            if (Math.Abs(dx) <=1 && Math.Abs(dy) <=1)
+            if (Math.Abs(dx) <= 1 && Math.Abs(dy) <= 1)
             {
                 // head and tail are adjacent, return original tail position
                 return tail;
@@ -87,26 +133,22 @@
                 y = dy > 0 ? y + 1 : y - 1;
                 if (dx != 0) // not in same column, so move diagonally
                 {
-                    x = dx > 0 ? x + 1: x - 1;
+                    x = dx > 0 ? x + 1 : x - 1;
                 }
             }
 
             return (x, y);
         }
 
-
-        private List<(char, int)> GetMoveList()
+        public override string ToString()
         {
-            List<(char,int)> moves = new List<(char,int)>();
-            foreach (string line in File.ReadLines(_inputFile))
+            var output = string.Empty;
+            foreach (var knot in _knots)
             {
-                char direction = line[0];
-                int moveCount = int.Parse(line.Substring(2));
-                moves.Add((direction,moveCount));
+                output += knot.ToString() + ", ";
             }
-            return moves;
+
+            return output;
         }
-
     }
-
 }
