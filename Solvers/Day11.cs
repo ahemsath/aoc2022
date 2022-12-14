570cbaa1bbf7b00b2aa1092c5671135a01e07e76
Alex Hemsath <alex.hemsath@gmail.com>
2022-12-13 17:35:23 -0800

    Day 2 complete
    1. Had to refactor to initialize the list of monkeys at the start of each answer.
    2. Refactor to use long instead of int for item values.
    3. I had help in figuring out the trick necessary to solving this one without a supercomputer and five years. :-)

--- Solvers/Day11.cs
+++ Solvers/Day11.cs
@@ -1,18 +1,60 @@
-﻿using System.Security.Cryptography;
-using System;
-
-namespace Solvers
+﻿namespace Solvers
 {
-
     public class Day11 : ISolver
     {
         private string _inputFile;
-        private List<Monkey> _monkeyList;
 
         public Day11(string inputFile)
         {
             _inputFile = inputFile;
-            _monkeyList = new List<Monkey>();
+        }
+
+        public string Answer1()
+        {
+            var monkeyList = GetMonkeys(false);
+
+            for (int round = 0; round < 20; round++)
+            {
+                foreach(var monkey in monkeyList)
+                {
+                    var throwList = monkey.TakeTurn();
+                    foreach ((long item, int targetMonkey) in throwList)
+                    {
+                        monkeyList[targetMonkey].Catch(item);
+                    }
+                }
+            }
+
+            // calculate "monkey business"
+            var inspectionCounts = monkeyList.Select(x => x.InspectionCount).ToList().OrderByDescending(x => x).ToArray();
+            return (inspectionCounts[0] * inspectionCounts[1]).ToString();
+        }
+
+        public string Answer2()
+        {
+            var monkeyList = GetMonkeys(true);
+
+            for (int round = 0; round < 10000; round++)
+            {
+                foreach (var monkey in monkeyList)
+                {
+                    var throwList = monkey.TakeTurn();
+                    foreach ((long item, int targetMonkey) in throwList)
+                    {
+                        monkeyList[targetMonkey].Catch(item);
+                    }
+                }
+            }
+
+            // calculate "monkey business"
+            var inspectionCounts = monkeyList.Select(x => x.InspectionCount).ToList().OrderByDescending(x => x).ToArray();
+            return (inspectionCounts[0] * inspectionCounts[1]).ToString();
+        }
+
+        private List<Monkey> GetMonkeys(bool scary)
+        {
+            var monkeyList = new List<Monkey>();
+            var productOfDivisors = 1;
             var lines = File.ReadAllLines(_inputFile);
             int index = 0;
             while (index + 5 < lines.Length)
@@ -30,7 +72,7 @@ namespace Solvers
 
                 // read input items
                 string startingItemsList = lines[index].Split(':')[1].Trim();
-                List<int> startingItems = startingItemsList.Split(",").Select(x => int.Parse(x.Trim())).ToList();
+                List<long> startingItems = startingItemsList.Split(",").Select(x => long.Parse(x.Trim())).ToList();
                 index++;
 
                 // read operation
@@ -39,6 +81,7 @@ namespace Solvers
 
                 // read test divisor
                 int testDivisor = int.Parse(lines[index].Split(':')[1].Trim().Replace("divisible by ", ""));
+                productOfDivisors *= testDivisor;
                 index++;
 
                 // read monkey to throw item to if test passes
@@ -49,51 +92,42 @@ namespace Solvers
                 int falseMonkey = int.Parse(lines[index].Split(':')[1].Trim().Replace("throw to monkey ", ""));
                 index++;
 
-                _monkeyList.Add(new Monkey(startingItems, operation, testDivisor, trueMonkey, falseMonkey));
+                monkeyList.Add(new Monkey(startingItems, operation, testDivisor, trueMonkey, falseMonkey));
 
                 // skip blank line before next monkey, if any
                 index++;
             }
-        }
 
-        public string Answer1()
-        {
-            for (int round = 0; round < 20; round++)
+            if (scary)
             {
-                foreach(var monkey in _monkeyList)
+                foreach (var monkey in monkeyList)
                 {
-                    var throwList = monkey.TakeTurn();
-                    foreach ((int item, int targetMonkey) in throwList)
-                    {
-                        _monkeyList[targetMonkey].Catch(item);
-                    }
+                    monkey.GetScary(productOfDivisors);
                 }
             }
 
-            // calculate "monkey business"
-            var inspectionCounts = _monkeyList.Select(x => x.InspectionCount).ToList().OrderByDescending(x => x).ToArray();
-            return (inspectionCounts[0] * inspectionCounts[1]).ToString();
-        }
-
-        public string Answer2()
-        {
-            return string.Empty;
+            return monkeyList;
         }
 
     }
 
     public class Monkey
     {
-        private List<int> _items;
+        private List<long> _items;
         private IOperation _operation;
         private int _testDivisor;
         private int _trueMonkey;
         private int _falseMonkey;
 
-        private int _inspectionCount;
-        public int InspectionCount => _inspectionCount;
+        private bool _simpleWorryReduction;
+        private int _worryReductionModulus;
+        
 
-        public Monkey(List<int> items, string operation, int testDivisor, int trueMonkey, int falseMonkey)
+        private long _inspectionCount;
+        public long InspectionCount => _inspectionCount;
+        public int Divisor => _testDivisor;
+
+        public Monkey(List<long> items, string operation, int testDivisor, int trueMonkey, int falseMonkey)
         {
             _items = items;
             _operation = ParseOperation(operation);
@@ -101,26 +135,49 @@ namespace Solvers
             _trueMonkey = trueMonkey;
             _falseMonkey = falseMonkey;
             _inspectionCount = 0;
+            _simpleWorryReduction = true;
+            _worryReductionModulus = 0;
         }
 
         // The output of this method is a list of items paired with the monkey they are being thrown to
         // The reason we can't throw the items from the Monkey class is that it has no reference the the list of monkeys
-        public List<(int item, int monkey)> TakeTurn()
+        public List<(long item, int monkey)> TakeTurn()
         {
-            var output = new List<(int item, int monkey)>();
+            var output = new List<(long item, int monkey)>();
 
             foreach(var item in _items)
             {
                 // inspect item
                 _inspectionCount++;
                 // perform operation
-                var newWorryLevel = _operation.Apply(item) / 3;
+                var newWorryLevel = _operation.Apply(item);
+                if (_simpleWorryReduction)
+                {
+                     newWorryLevel /= 3;
+                }
+                else
+                {
+                    // The trick to handling the explosion in worry levels caused by no longer dividing new worry levels by three, and also taking 10,000 rounds,
+                    // is to perform a modulo operation on the new worry level, where the modulus is the product of all monkeys' test divisors. This works because:
+                    // 1. The main thing is to keep track of which monkey each item is thrown to during each monkey's turn
+                    // 2. The thing that determines 1. for each monkey is whether it is evenly divisible by its test divisor
+                    // 3. The answer to 2. is remains correct for all monkeys when you only keep track of the remainder of dividing by the product of all monkeys' test divisors.
+
+                    // I definitely did not figure this out on my own. I got a hint from Reddit that all we really care about is the divisibility test for each item. I had a hunch
+                    // that modulating by the common product of all divisors was correct, and I consulted a coworker's solution to confirm.
+
+                    // We still need to use 64-bit (long) integers to avoid overflow, even in the sample input case, because the common product of all divisors is 96,577, which
+                    // makes the max value of each item one less than that number, which can end up being squared if it ends up being thrown to a monkey with that operation,
+                    // and 96,576^2 is way too big to fit in a 32-bit integer.
+
+                    newWorryLevel = newWorryLevel % _worryReductionModulus;
+                }
 
                 output.Add((newWorryLevel, newWorryLevel % _testDivisor == 0 ? _trueMonkey : _falseMonkey));
             }
 
             // All items have been thrown at this point so empty out the item list
-            _items = new List<int>();
+            _items = new List<long>();
 
             return output;
         }
@@ -146,16 +203,21 @@ namespace Solvers
             }
         }
 
-        public void Catch(int item)
+        public void Catch(long item)
         {
             _items.Add(item);
         }
 
+        public void GetScary(int worryReductionModulus)
+        {
+            _simpleWorryReduction = false;
+            _worryReductionModulus = worryReductionModulus;
+        }
     }
 
     public interface IOperation
     {
-        int Apply(int input);
+        long Apply(long input);
     }
 
     public class Add : IOperation
@@ -167,7 +229,7 @@ namespace Solvers
             _operand = operand;
         }
 
-        public int Apply(int input)
+        public long Apply(long input)
         {
             return input + _operand;
         }
@@ -182,7 +244,7 @@ namespace Solvers
             _operand = operand;
         }
 
-        public int Apply(int input)
+        public long Apply(long input)
         {
             return input * _operand;
         }
@@ -194,7 +256,7 @@ namespace Solvers
         {
         }
 
-        public int Apply(int input)
+        public long Apply(long input)
         {
             return input * input;
         }
